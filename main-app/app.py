5a70bce42bf9d42b2aa45148c10c2821b5cd6a43
gorgick <gorgick2014@yandex.by>
2026-04-27 17:24:27 +0300

    declarativebase deribit created with run

--- main-app/app.py
+++ main-app/app.py
@@ -4,10 +4,13 @@ from fastapi import FastAPI
 
 from api import router
 from core import db_helper
+from tables import Deribit
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Deribit.metadata.create_all)
     yield
     await db_helper.dipose()
 
